d7c14958fad1858f60646b65e52366e50a889c04
Suepaphly <tabenterprise@gmail.com>
2021-10-14 15:58:37 -0500

    Update doom.ts

--- src/app/modules/doom/doom.ts
+++ src/app/modules/doom/doom.ts
@@ -1,1 +1,14 @@
+import {
+    BaseModule,
+    codec,
+    AfterBlockApplyContext,
+    TransactionApplyContext,
+    BeforeBlockApplyContext,
+    AfterGenesisBlockApplyContext,
+    // GenesisConfig
+} from 'lisk-sdk';
 
+
+export class HelloModule extends BaseModule {
+
+}
