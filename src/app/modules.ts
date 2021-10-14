c48045cb16ca283edc385c793cd948999f8e8457
Suepaphly <tabenterprise@gmail.com>
2021-10-14 16:23:48 -0500

    Update modules.ts

--- src/app/modules.ts
+++ src/app/modules.ts
@@ -1,9 +1,9 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
 import { Application } from 'lisk-sdk';
-import { doomonlisk } from "./modules/doomonlisk/doomonlisk_module";
+import { DoomonliskModule } from "./modules/doomonlisk/doomonlisk_module";
 
 // @ts-expect-error Unused variable error happens here until at least one module is registered
 export const registerModules = (app: Application): void => {
 
-    app.registerModule(doomonlisk);
+    app.registerModule(DoomonliskModule);
 };
