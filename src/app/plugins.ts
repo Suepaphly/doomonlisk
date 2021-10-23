2a1db86bc83da1c5ad4e741ac9e62a2bf671d63a
Suepaphly <tabenterprise@gmail.com>
2021-10-23 15:46:08 -0500

    Update plugins.ts

--- src/app/plugins.ts
+++ src/app/plugins.ts
@@ -1,9 +1,9 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
 import { Application } from 'lisk-sdk';
-import { playdoom } from "./plugins/playdoom/playdoom_plugin";
+import { PlaydoomPlugin } from "./plugins/playdoom/playdoom_plugin";
 
 // @ts-expect-error Unused variable error happens here until at least one module is registered
 export const registerPlugins = (_app: Application): void => {
   
-    _app.registerPlugin(playdoom);
+    _app.registerPlugin(PlaydoomPlugin);
 };
