6228938fd7c284c334a4235f29dd3938010d7dc3
Suepaphly <tabenterprise@gmail.com>
2021-10-25 13:45:47 -0500

    Update doomonlisk_module.ts

--- src/app/modules/doomonlisk/doomonlisk_module.ts
+++ src/app/modules/doomonlisk/doomonlisk_module.ts
@@ -56,31 +56,31 @@ export class DoomonliskModule extends BaseModule {
 		ci.events().onFrame((frame) => {
 		    rgb = frame;
 		});
+		
+		    while(true){
+			setTimeout(() => {
+			    const width = ci.width();
+			    const height = ci.height();
 
-		// capture the screen after 3 sec
-		console.log("Will capture screen after 3 sec...");
-   		setTimeout(() => {
-		    const width = ci.width();
-		    const height = ci.height();
+			    const rgba = new Uint8Array(width * height * 4);
+			    for (let next = 0; next < width * height; ++next) {
+				rgba[next * 4 + 0] = rgb[next * 3 + 0];
+				rgba[next * 4 + 1] = rgb[next * 3 + 1];
+				rgba[next * 4 + 2] = rgb[next * 3 + 2];
+				rgba[next * 4 + 3] = 255;
+			    }
 
-		    const rgba = new Uint8Array(width * height * 4);
-		    for (let next = 0; next < width * height; ++next) {
-			rgba[next * 4 + 0] = rgb[next * 3 + 0];
-			rgba[next * 4 + 1] = rgb[next * 3 + 1];
-			rgba[next * 4 + 2] = rgb[next * 3 + 2];
-			rgba[next * 4 + 3] = 255;
+					console.log("processed");
+			    new jimp({ data: rgba, width, height }, (err, image) => {
+				image.write("./src/app/build/screens/screenshot.png", () => {
+
+					console.log("Captured");
+					ci.exit();
+
+				});
+			    });	
+			}, 28); //capture timer
 		    }
-			
-				console.log("processed");
-		    new jimp({ data: rgba, width, height }, (err, image) => {
-			image.write("./src/app/build/screens/screenshot.png", () => {
-			    
-				console.log("Captured");
-				ci.exit();
-				
-			});
-		    });	
-        	}, 28); //capture timer
 	    })
 	    .catch(console.error);	 
      }
