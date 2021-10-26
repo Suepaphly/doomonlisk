6d547d12dfb1f02237b0b89a0a4ee45794616e9c
Suepaphly <tabenterprise@gmail.com>
2021-10-26 03:12:23 -0500

    comment the right thing out

--- src/app/modules/doomonlisk/doomonlisk_module.ts
+++ src/app/modules/doomonlisk/doomonlisk_module.ts
@@ -35,7 +35,7 @@ export class DoomonliskModule extends BaseModule {
 			    const width = ci.width();
 			    const height = ci.height();
 
-			    //const rgba = new Uint8Array(width * height * 4);
+			    const rgba = new Uint8Array(width * height * 4);
 			    for (let next = 0; next < width * height; ++next) {
 				rgba[next * 4 + 0] = rgb[next * 3 + 0];
 				rgba[next * 4 + 1] = rgb[next * 3 + 1];
@@ -43,7 +43,7 @@ export class DoomonliskModule extends BaseModule {
 				rgba[next * 4 + 3] = 255;
 			    }
 
-                this.currentFramecurrentFrame = rgba;
+                //this.currentFramecurrentFrame = rgba;
 			    new jimp({ data: rgba, width, height }, (err, image) => {
 				image.write("./src/app/build/screens/screenshot0.png", () => {
 					console.log(frameCount); 
