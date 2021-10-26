92dd63dba3894b9a92e536f74f6087310e0892a8
Suepaphly <tabenterprise@gmail.com>
2021-10-25 19:05:51 -0500

    Update playdoom_plugin.ts

--- src/app/plugins/playdoom/playdoom_plugin.ts
+++ src/app/plugins/playdoom/playdoom_plugin.ts
@@ -85,7 +85,7 @@ interface PlayDoomPluginOptions extends PluginOptionsWithAppConfig {
 
 		public async load(channel: BaseChannel): Promise<void> {
 		this._channel = channel;
-		this._channel.once('doomonlisk:getFrame', (frame) => {console.log(frame);});
+		this._channel.once('getFrame', (frame) => {console.log(frame);});
 	
 			
 		
