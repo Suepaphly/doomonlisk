b42e0ea6487a0c13bae2af22e7db14e5c6350be0
Suepaphly <tabenterprise@gmail.com>
2021-10-25 19:56:55 -0500

    Update doomonlisk_module.ts

--- src/app/modules/doomonlisk/doomonlisk_module.ts
+++ src/app/modules/doomonlisk/doomonlisk_module.ts
@@ -54,7 +54,7 @@ const currentFrame = emulators
 
 export class DoomonliskModule extends BaseModule {
     public actions = {
-	//getFrame: async () => { return 1; }, 
+	getFrame: async () => {  }, 
 		 
 		 
 	 
