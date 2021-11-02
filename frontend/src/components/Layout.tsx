63dee775292750165eefdc09b1f5788c3da1bde5
Suepaphly <tabenterprise@gmail.com>
2021-11-02 06:37:58 -0500

    Update Layout.tsx

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,21 +1,12 @@
-import { useContext, useState } from "react";
-import { Card, Layout, Menu } from "antd";
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Layout } from "antd";
 import { useHistory } from "react-router";
-import { LoginOutlined, LogoutOutlined } from "@ant-design/icons";
 
 const { Header, Content, Footer } = Layout;
 
 
-const maxWidth = "1200px";
-
 const AppLayout: React.FC = ({ children }) => {
-  const [selectedItem, setSelectedItem] = useState("home");
   const history = useHistory();
-
-  const handleLogout = () => {
-    history.push(`/`);
-  };
 
   return (
     <Layout>
