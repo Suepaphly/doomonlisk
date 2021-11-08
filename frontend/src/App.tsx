9ca023ea8be4559d0f730846f50ca57294e1d4c5
Suepaphly <tabenterprise@gmail.com>
2021-11-08 15:16:55 -0600

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,5 @@
 import Routes from "./Routes";
 import AppLayout from "./components/Layout";
-import React, { useState, useEffect, useRef } from "react";
 import * as api from "./utils/api";
 import * as jimp from 'jimp';
 
@@ -10,30 +9,27 @@ const frameRefreshRate = 10000;
 
 const App: React.FC = () => {
   
-  const canvasRef = document.getElementById('myImage');
-  
-  const [frame, setFrame] = useState([]);
+  const canvasRef = document.getElementById('myImage');  
 
   useEffect(() => {
-    const intervalId = setInterval(RefreshFrame, frameRefreshRate);
-    RefreshFrame();
+      const intervalId = setInterval(function () => {
+      const nextFrame = await api.getFrame();        
+
+      if(canvasRef.current){
+        const canvas = canvasRef.current;
+        const ctx = canvas.getContext('2d');
+      }
+
+      const imageData = new ImageData(Uint8ClampedArray.from(nextFrame), 320, 200);
+      ctx.putImageData(imageData, 0, 0); 
+    }, frameRefreshRate);
+     
 
     return () => {
       clearInterval(intervalId);
     };
   }, []);  
   
-  const RefreshFrame = async () => {
-    const nextFrame = await api.getFrame();        
-    
-    if(canvasRef.current){
-      const canvas = canvasRef.current;
-      const ctx = canvas.getContext('2d');
-    }
-    
-    const imageData = new ImageData(Uint8ClampedArray.from(nextFrame), 320, 200);
-    ctx.putImageData(imageData, 0, 0);
-  };
   
   
   
