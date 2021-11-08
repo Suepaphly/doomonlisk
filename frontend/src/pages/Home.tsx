da1a3d01142d22f217add413ad84c618b711cfdf
Suepaphly <tabenterprise@gmail.com>
2021-11-08 11:38:19 -0600

    Update Home.tsx

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,8 +1,14 @@
 import Home from "../components/Home";
+import * as api from "../utils/api";
+
+const client = await api.getClient();
+
+const frame = client.getFrame();
 
 export default function HomePage() {
   return (
     <div>
+      ${frame}
       <Home />
     </div>
   );
