bf787f23320759e8a786406c4fdefdbb2bb860c3
Suepaphly <tabenterprise@gmail.com>
2021-11-08 12:22:41 -0600

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -17,7 +17,7 @@ const App: React.FC = () => {
   }, []);
 
   const refreshFrame = async () => {
-    const nextFrame = await fetchnextFrame();    
+    const nextFrame = await api.getFrame();    
 
     setFrame(nextFrame);
   };
