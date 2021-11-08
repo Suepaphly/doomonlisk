583089ca57fc534a99ba5e8f74d1e4c8e2c8284f
Suepaphly <tabenterprise@gmail.com>
2021-11-08 12:17:40 -0600

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,29 @@ import Routes from "./Routes";
 import AppLayout from "./components/Layout";
 
 const App: React.FC = () => {
+  
+  const [frame, setFrame] = useState([]);
 
+  useEffect(() => {
+    const intervalId = setInterval(refreshFrame, statsRefreshRate);
+    refreshStats();
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
+  const refreshFrame = async () => {
+    const nextFrame = await fetchnextFrame();    
+
+    setFrame(nextFrame);
+  };
+
+
+  
+  
+  
+  
   return (
       <AppLayout>
         <Routes />
