4bb7f66c3c6d379bd15993fe647012cd0761e771
Suepaphly <tabenterprise@gmail.com>
2021-11-08 14:13:34 -0600

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -43,7 +43,7 @@ const App: React.FC = () => {
   
   return (
       <AppLayout>
-         <canvas ref={canvasRef} {frame}/>
+         <canvas ref={canvasRef} {...frame}/>
         <Routes />
       </AppLayout>
   );
