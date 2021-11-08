4e3ff774c0839f19f1427def9a89493a840d8a4e
Suepaphly <tabenterprise@gmail.com>
2021-11-08 15:17:55 -0600

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -12,7 +12,7 @@ const App: React.FC = () => {
   const canvasRef = document.getElementById('myImage');  
 
   useEffect(() => {
-      const intervalId = setInterval(function () => {
+      const intervalId = setInterval(function () {
       const nextFrame = await api.getFrame();        
 
       if(canvasRef.current){
