485ce7696f6c3cfe04dded69b38d33943f0f6f52
Suepaphly <tabenterprise@gmail.com>
2021-11-08 14:15:10 -0600

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,7 +11,7 @@ const frameRefreshRate = 10000;
 const App: React.FC = () => {
   
   
-  const canvasRef = useRef(null)
+  const canvasRef = useRef<HTMLDivElement>(null)
   const canvas = canvasRef.current
   const ctx = canvas.getContext('2d')
 
