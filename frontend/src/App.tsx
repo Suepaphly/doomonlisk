a3b9b7932cdf5222778e528fa89608054e9e4e08
Suepaphly <tabenterprise@gmail.com>
2021-11-09 06:10:41 -0600

    chantunit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,17 +4,12 @@ import AppLayout from "./components/Layout";
 import * as api from "./utils/api";
 import * as jimp from 'jimp';
 
-
 const frameRefreshRate = 10000;
-
-
 
 const App: React.FC = () => {
   
   const canvasRef = useRef<HTMLCanvasElement>(null);
-  const [frame, setFrame] = useState(new Uint8Array(2));
-
-
+  const [frame, setFrame] = useState(new Uint8Array(256000));
 
   useEffect (()  => {    
     if (canvasRef.current) {
@@ -31,37 +26,7 @@ const App: React.FC = () => {
   const refreshFrame = async () => {
     const nextFrame = await api.getFrame()
     setFrame(nextFrame);
-  }       
-
-  /*
-  useEffect(() => {
-      const intervalId = setInterval(async function () {
-      const nextFrame = await api.getFrame();        
-
-      if(canvasRef?.current){
-        const canvas = canvasRef.current;
-        const ctx = canvas.getContext('2d');
-        
-        if(ctx != null){
-          const imageData = new ImageData(Uint8ClampedArray.from(nextFrame), 320, 200);
-          ctx.putImageData(imageData, 0, 0); 
-        }
-
-      }
-
-      
-    }, frameRefreshRate);
-     
-     
-
-    return () => {
-      clearInterval(intervalId);
-    };
-  }, []);   */
-  
-  
-  
-  
+  }  
   
   return (
          <canvas id="myImage" ref={canvasRef} />
