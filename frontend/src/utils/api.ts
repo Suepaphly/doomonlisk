ec7a9fbc4b1c94443d6ecb5efb90b01497d5cb0f
Suepaphly <tabenterprise@gmail.com>
2021-11-10 00:56:16 -0600

    change ip

--- frontend/src/utils/api.ts
+++ frontend/src/utils/api.ts
@@ -4,7 +4,7 @@ let clientCache : APIClient;
 
 const getClient = async () => {
   if (!clientCache) {
-    clientCache = await apiClient.createWSClient('ws://localhost:8000/ws');
+    clientCache = await apiClient.createWSClient('ws://45.77.75.247:8000/ws');
   }
   return clientCache;
 };
