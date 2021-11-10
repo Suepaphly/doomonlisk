3b2b6e30169918a16ae8db5ad2e8647fbf3f6018
Suepaphly <tabenterprise@gmail.com>
2021-11-10 01:25:38 -0600

    change port

--- frontend/src/utils/api.ts
+++ frontend/src/utils/api.ts
@@ -4,7 +4,7 @@ let clientCache : APIClient;
 
 const getClient = async () => {
   if (!clientCache) {
-    clientCache = await apiClient.createWSClient('ws://45.77.75.247:80/ws');
+    clientCache = await apiClient.createWSClient('ws://45.77.75.247:8124/ws');
   }
   return clientCache;
 };
