5ca03df4480e162a58962239a642d5da1cb75998
Suepaphly <tabenterprise@gmail.com>
2021-11-11 17:38:46 -0600

    refactor

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,18 +6,47 @@ import * as jimp from 'jimp';
 
 const frameRefreshRate = 10000;
 
+const client = await api.getClient();
+
+
 const App: React.FC = () => {
   
   const canvasRef = useRef<HTMLCanvasElement>(null);
+  const frameRef = useRef(null);
+
+
+
+  useEffect (()  => {    
+    setInterval(()=>{ frameRef.current = client.invoke<Uint8Array>("doomonlisk:getFrame") }, 1000 );
+    window.requestAnimationFrame(drawFrame);
+  }, []);
+
+
+
+  const drawFrame = async () => {
+    if (canvasRef.current) {
+      refreshFrame();
+      const canvas = canvasRef.current;
+      const context = canvas.getContext('2d');
+      if (context && frameRef.current) {
+        
+        var imageData = new ImageData(Uint8ClampedArray.from(Object.values(frameRef.current)), 320, 200);
+
+        context.clearRect(0, 0, 320, 200);
+        context.putImageData(imageData, 0, 0); 
+        window.requestAnimationFrame(drawFrame);
+      }
+  }
+
+
+  /*
   const [frame, setFrame] = useState(new Uint8Array(256000));
 
   useEffect (()  => {    
 
     const intervalId = setInterval(refreshFrame, frameRefreshRate);
 
-
     window.requestAnimationFrame(drawFrame);
-
 
     return () => {
       clearInterval(intervalId);
@@ -43,12 +72,13 @@ const App: React.FC = () => {
         window.requestAnimationFrame(drawFrame);
       }
   }
-  
   }
+  */
   
   return (
          <canvas id="myImage" ref={canvasRef} />
   );
-};
+  };
+}
 
 export default App;
