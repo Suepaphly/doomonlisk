9a6a0cc3e06c82b43d1f4eb6a99659fd0efe4cd5
Suepaphly <tabenterprise@gmail.com>
2021-11-11 21:12:47 -0600

    it works lololol

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -26,7 +26,7 @@ const App: React.FC = () => {
         let frameData = await clientRef.current.invoke("doomonlisk:getFrame");
         drawFrame(frameData);
       }    
-    }, 1000 );
+    }, 30 );
 
   }, []);
 
