450aa4e495e28ab70a8260b468b4a6217e3e33db
Suepaphly <tabenterprise@gmail.com>
2021-11-14 12:57:21 -0600

    remove cast

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -35,7 +35,7 @@ const App: React.FC = () => {
   const handler = async (event : any) => {
     if(clientRef.current){
       let testKey = await clientRef.current.invoke("doomonlisk:pEnter");
-      console.log(string(event.code));
+      console.log(event.code);
     }       
   };
 
