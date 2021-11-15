3b28c9f80be015ce9ef8edc84ce0aff96b0448c3
Suepaphly <tabenterprise@gmail.com>
2021-11-15 07:26:45 -0600

    update asset

--- backend/src/app/modules/doomonlisk/tx/unlock.tsx
+++ backend/src/app/modules/doomonlisk/tx/unlock.tsx
@@ -15,23 +15,12 @@ class unlockControls extends BaseAsset {
 		transaction,
 		stateStore,
 	}) {
-        const sender = await stateStore.account.get(transaction.senderAddress);
-        if (sender.srs.config && sender.srs.config.friends.length !== 0) {
-            throw Error('Account already has a recovery configuration.')
-        }
-        const sameAccount = asset.friends.find(f => f === sender.address);
-        if (sameAccount) {
-            throw new Error('You cannot add yourself to your own friend list.');
-        }
-        // Add friends to the list
-        sender.srs.config.friends = [...asset.friends.sort()];
-        // Minimum number of friends required to vouch
-        sender.srs.config.recoveryThreshold = asset.recoveryThreshold;
-        // Minimum number of blocks after recovery process when account will be recoverable
-        sender.srs.config.delayPeriod = asset.delayPeriod;
-        // Set the deposit based on number of friends, 10 + friends.length * 2
-        const deposit = BigInt(BASE_RECOVERY_DEPOSIT) + BigInt(transactions.convertLSKToBeddows((sender.srs.config.friends.length * FRIEND_FACTOR_FEE).toString()));
-        sender.srs.config.deposit = deposit;
+
+        // Add blocks to the timer
+        sender.doomonlisk.config.blocksLeft = 0;
+        // Add default name
+        sender.doomonlisk.config.playerName = "Anonymous";
+       
         // Save the value in stateStore
         await stateStore.account.set(sender.address, sender);
     }
