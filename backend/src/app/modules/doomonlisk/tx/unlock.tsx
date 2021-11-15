421593912bfff45b6a20ad646f3e0aa02795a0d5
Suepaphly <tabenterprise@gmail.com>
2021-11-15 06:48:07 -0600

    addvariables

--- backend/src/app/modules/doomonlisk/tx/unlock.tsx
+++ backend/src/app/modules/doomonlisk/tx/unlock.tsx
@@ -1,8 +1,14 @@
 const { BaseAsset } = require("lisk-sdk");
+const { createRecoverySchema } = require('../schemas');
 
 // extend base asset to implement the custom asset
 class unlockControls extends BaseAsset { 
+  
+    name = "unlockControls"; 
+    id = 0; 
+    schema = createRecoverySchema;
 
+    
 }
 
 module.exports = unlockControls; 
